c15745dad983ebef5258099380c2f7dbddeda300
ardhianww <ardhwibawa7@gmail.com>
2025-03-01 05:24:29 +0700

    hapus beberapa file

--- hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-async function generateHash() {
-  const hashedPassword = await bcrypt.hash("password123", 10);
-  console.log("Hashed Password:", hashedPassword);
-}
-
-generateHash();
